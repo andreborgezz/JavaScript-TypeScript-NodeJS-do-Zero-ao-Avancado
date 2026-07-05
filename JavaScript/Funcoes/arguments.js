98d64c351d5c172920bf5598f3a83919bb4b52be
andreborgezz <andre06borges06@gmail.com>
2026-07-05 20:49:30 -0300

    fim do dia 05/07

--- JavaScript/Funcoes/arguments.js
+++ JavaScript/Funcoes/arguments.js
@@ -1,5 +1,15 @@
 function soma(num1, num2){ // Parametros são criados dentro do ()
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+        
+    }
+    return "Você passou dados diferentes";
 }
 
-console.log(soma(10, 20)) // Atribuimos valor dentro do () quando criamos
+console.log(soma(1,4)) // Atribuimos valor dentro do () quando criamos
+
+function subtracao() {
+    return arguments;
+}
+
+console.log(subtracao(1,2))
